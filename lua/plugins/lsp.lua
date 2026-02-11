fc30a488797191affb2f7a17e4eedef95848149c
Ethan Bailey <ebailey256@gmail.com>
2026-02-11 11:33:01 -0500

    add ruff organize imports

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -45,4 +45,15 @@ return {
       },
     },
   },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        python = {
+          'ruff_organize_imports',
+          'ruff_format',
+        },
+      },
+    },
+  },
 }
