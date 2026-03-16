8a31dad1f4960964892931952e4122c9dea18766
Ethan Bailey <ebailey256@gmail.com>
2026-03-16 14:09:23 -0400

    fix repeat scrolling glitch

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,12 @@ return {
   'folke/snacks.nvim',
   opts = {
     dashboard = { enabled = false },
+    scroll = {
+      animate_repeat = {
+        delay = 100,
+        duration = { step = 1, total = 1 },
+        easing = 'linear',
+      },
+    },
   },
 }
